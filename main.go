16d8126c25819c7dbfff115bd029c1f759bdb039
ariichi88 <ariichi88@gmail.com>
2020-04-07 18:55:43 +0900

    大幅に書き直し

--- main.go
+++ main.go
@@ -8,35 +8,37 @@ import (
 	"time"
 	"unicode"
 
-	"github.com/jessevdk/go-flags"
+    flags "github.com/jessevdk/go-flags"
 )
 
-// 和暦を西暦に変換
-func convertToChristianCal(date string, isLong bool) {
+// 文字列から日付及び元号を取り出す
+func getDateAndEra(date string) (y, m, d, era int) {
 
 	var (
 		isDigit     = false
 		str         = ""
-		magicNumber int
-		y, m, d     int
 		dateStr     []string
 	)
 
-	// どの年号か判別してmagicNumberを求める
-	if strings.ContainsAny(date, "明治MmＭｍ") {
-		magicNumber = 1867 // 明治元年は西暦1868年
-	} else if strings.ContainsAny(date, "大正TtＴｔ") {
-		magicNumber = 1911 // 大正元年は西暦1912年
-	} else if strings.ContainsAny(date, "昭和SsＳｓ") {
-		magicNumber = 1925 // 昭和元年は西暦1926年
-	} else if strings.ContainsAny(date, "平成HhＨｈ") {
-		magicNumber = 1988 // 平成元年は西暦1989年
-	}
+	// どの年号か判別してera（元号)を求める
+	if strings.ContainsAny(date, "明MmＭｍ") {
+		era = 1 // 明治
+	} else if strings.ContainsAny(date, "大TtＴｔ") {
+		era = 2 // 大正
+	} else if strings.ContainsAny(date, "昭SsＳｓ") {
+		era = 3 // 昭和
+	} else if strings.ContainsAny(date, "平HhＨｈ") {
+		era = 4 // 平成
+	} else if strings.ContainsAny(date, "令RrＲｒ") {
+        era = 5 // 令和
+    } else {
+        era = 0 // 西暦
+    }
 
-	// dateの前処理(最後が文字でないと次の処理で最後の数字が取り出せないため)
+    // dateの前処理(最後が文字でないと次の処理で最後の数字が取り出せないため)
 	date = date + "."
 
-	// 与えられた文字列から数字の部分だけ取り出す
+    // 与えられた文字列から数字の部分だけ取り出す
 	for _, char := range date {
 		if unicode.IsDigit(char) {
 			str = str + string(char)
@@ -53,127 +55,137 @@ func convertToChristianCal(date string, isLong bool) {
 		}
 	}
 
-	// 日付を正しく取得できてなかった場合(dateStrの長さが３でない)は終了
+	// 日付を正しく取得できてなかった場合(dateStrの長さが３でない)
 	if len(dateStr) != 3 {
-		fmt.Println("日付の取得に失敗しました")
-		os.Exit(1)
-	}
+       fmt.Println("日付の取得に失敗しました")
+       os.Exit(1)
+    }
 
 	// 日付を取得
 	y, _ = strconv.Atoi(dateStr[0])
 	m, _ = strconv.Atoi(dateStr[1])
 	d, _ = strconv.Atoi(dateStr[2])
 
-	// 西暦の日付を返す
-	if isLong {
-		fmt.Printf("%d年%d月%d日\n", y+magicNumber, m, d)
-	} else {
-		fmt.Printf("%d/%d/%d\n", y+magicNumber, m, d)
-	}
+    return y, m, d, era
+}
 
+
+// 和暦を西暦に変換
+func convertToChristianCal(y, m , d, era int, kanji bool) string {
+
+    var magicNumber int
+
+    switch era {
+    case 1: // 明治
+        magicNumber = 1868
+    case 2: // 大正
+        magicNumber = 1911
+    case 3: // 昭和
+        magicNumber = 1925
+    case 4: // 平成
+        magicNumber = 1988
+    case 5: // 令和
+        magicNumber = 2018
+    }
+
+    year := strconv.Itoa(y + magicNumber)
+    month := strconv.Itoa(m)
+    date := strconv.Itoa(d)
+
+    if kanji {
+        return year + "年" + month + "月" + date + "日"
+    } else {
+        return year + "/" + month + "/" + date
+    }
 }
 
 // 西暦を和暦に変換
-func convertToJapaneseCal(date string, isLong, isGrif bool) {
+func convertToJapaneseCal(y, m, d int, kanji bool) string {
 
-	var (
-		t           time.Time
-		magicNumber int
-		eraNameL    string
-		eraNameS    string
-		y, m, d     int
-	)
+    var (
+        magicNumber       int
+        name, nameK       string
+    )
 
-	// dateから日付を生成する
-	t, err := time.Parse("2006/01/02", date)
-	if err != nil {
-		t, err = time.Parse("2006/1/2", date)
-		if err != nil {
-			t, err = time.Parse("2006年01月02日", date)
-			if err != nil {
-				t, err = time.Parse("2006年1月2日", date)
-				if err != nil {
-					fmt.Println("日付の取得に失敗しました")
-					os.Exit(1)
-				}
-			}
-		}
-	}
+    // 日付の作成
+    t := time.Date(y, time.Month(m), d, 0, 0, 0, 0, time.Local)
 
-	// 元号を判定する
-	if t.Before(time.Date(1912, 7, 30, 0, 0, 0, 0, time.Local)) {
-		magicNumber = 1867
-		eraNameL = "明治"
-		eraNameS = "M"
-	} else if t.Before(time.Date(1926, 12, 25, 0, 0, 0, 0, time.Local)) {
-		magicNumber = 1911
-		eraNameL = "大正"
-		eraNameS = "T"
-	} else if t.Before(time.Date(1989, 1, 8, 0, 0, 0, 0, time.Local)) {
-		magicNumber = 1925
-		eraNameL = "昭和"
-		eraNameS = "S"
-	} else if t.Before(time.Date(2019, 5, 1, 0, 0, 0, 0, time.Local)) {
-		magicNumber = 1988
-		eraNameL = "平成"
-		eraNameS = "H"
-	}
+    // 元号を判定する
+    if t.Before(time.Date(1912, 7, 30, 0, 0, 0, 0, time.Local)) {
+        magicNumber = 1867
+        name = "M"
+        nameK = "明治"
+    } else if t.Before(time.Date(1926, 12, 25, 0, 0, 0, 0, time.Local)) {
+        magicNumber = 1911
+        name = "T"
+        nameK = "大正"
+    } else if t.Before(time.Date(1989, 1, 8, 0, 0, 0, 0, time.Local)) {
+        magicNumber = 1925
+        name = "S"
+        nameK = "昭和"
+    } else if t.Before(time.Date(2019, 5, 1, 0, 0, 0, 0, time.Local)) {
+        magicNumber = 1988
+        name = "H"
+        nameK = "平成"
+    } else {
+        magicNumber = 2018
+        name = "R"
+        nameK = "令和"
+    }
 
-	// 和暦の年月日の取得
-	y = t.Year() - magicNumber
-	m = int(t.Month())
-	d = t.Day()
+    year := strconv.Itoa(y - magicNumber)
+    if kanji {
+        if year == "1" {
+            year = nameK + "元"
+        } else {
+            year = nameK + year
+        }
+    } else {
+        year = name + year
+    }
 
-	// 和暦の年月日の表示
-	if isLong {
-		if isGrif && y == 1 {
-			fmt.Printf("%s元年%d月%d日\n", eraNameL, m, d)
-		} else {
-			fmt.Printf("%s%d年%d月%d日\n", eraNameL, y, m, d)
-		}
-	} else {
-		fmt.Printf("%s%d.%d.%d\n", eraNameS, y, m, d)
-	}
+    month := strconv.Itoa(m)
+    date := strconv.Itoa(d)
 
+    if kanji {
+        return year + "年" + month + "月" + date + "日"
+    } else {
+        return year + "." + month + "." + date
+    }
 }
-
-type options struct {
-	Jpn  bool `short:"j" description:"西暦を和暦に変換します"`
-	Chr  bool `short:"c" description:"和暦を西暦に変換します"`
-	Long bool `short:"l" description:"日付をy年m月d日で表示します"`
-	Grif bool `short:"g" description:"和暦の1年を元年で表示します(ｊオプションの時のみ)"`
-}
-
-var opt options
-
-var parser = flags.NewParser(&opt, flags.Default)
 
 func main() {
 
-	//　コマンド名と使用法の設定
-	parser.Name = "Era"
-	parser.Usage = "[OPTIONS] Date"
+    type option struct {
+        Kanji bool `short:"k" long:"kanji" description:"日付をＸ年Ｘ月Ｘ日の形式で返します"`
+    }
 
-	//　オプションのパース
+    var opt option
+
+    var parser = flags.NewParser(&opt, flags.Default)
+
+    parser.Name = "Era"
+    parser.Usage = "[OPTIONS] date"
+
 	args, err := parser.Parse()
 	if err != nil {
 		parser.WriteHelp(os.Stdout)
 		os.Exit(1)
 	}
 
-	// コマンドライン引数がないときはヘルプを出して終了
-	if len(args) == 0 {
+	if len(args) == 0 || len(args) > 1 {
 		parser.WriteHelp(os.Stdout)
 		os.Exit(1)
 	}
 
-	// オプションの解析とコマンドの実行
-	if opt.Jpn && opt.Chr {
-		fmt.Println("ｊオプションとｃオプションは同時に指定できません")
-		os.Exit(1)
-	} else if opt.Jpn {
-		convertToJapaneseCal(args[0], opt.Long, opt.Grif)
-	} else if opt.Chr {
-		convertToChristianCal(args[0], opt.Long)
-	}
+    y, m, d, era := getDateAndEra(args[0])
+
+    if era == 0 {
+        fmt.Println(convertToJapaneseCal(y, m, d, opt.Kanji))
+    } else {
+        fmt.Println(convertToChristianCal(y, m, d, era, opt.Kanji))
+    }
 }
+
+
+
